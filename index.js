a05243710430f2284ca26f942f19d1449caa5a1c
“Mateusz” <mateuszludyk@gmail.com>
2023-06-01 10:12:02 +0200

    Refactor, promises

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var fs = require('fs');
 var path = require('path');
 var yargs = require('yargs');
@@ -10,58 +46,121 @@ var extensions = {
     pictures: ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.raw', '.svg', '.webp', '.psd', '.ai', '.eps', '.ico', '.HEIC'],
     documents: ['.pdf', '.doc', '.docx', '.txt', '.xls', '.ppt', '.xlsx', '.pptx', '.odt', '.csv', '.xml', '.rtf'],
     video: ['.mp4', '.mov', '.avi', '.mkv', '.wmv', '.flv', '.webm'],
-    installerEx: ['.exe', '.dmg', '.pkg', '.deb', '.rpm']
+    installer: ['.exe', '.dmg', '.pkg', '.deb', '.rpm']
 };
 var musicDir = path.join(directoryPath, 'Music');
 var picDir = path.join(directoryPath, 'Pictures');
 var docDir = path.join(directoryPath, 'Document');
 var vidDir = path.join(directoryPath, 'Video');
 var instDir = path.join(directoryPath, 'Installers');
+function createDirectory(directory) {
+    return new Promise(function (resolve, reject) {
+        fs.mkdir(directory, { recursive: true }, function (error) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve();
+            }
+        });
+    });
+}
+function renameFile(sourcePath, destPath) {
+    return new Promise(function (resolve, reject) {
+        fs.rename(sourcePath, destPath, function (error) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve();
+            }
+        });
+    });
+}
 function organizeFiles(directoryPath) {
-    fs.readdir(directoryPath, function (error, files) {
-        if (error) {
-            console.log('Something went wrong: ', error);
-            return;
-        }
-        files.forEach(function (file) {
-            var filePath = path.join(directoryPath, file);
-            var fileName = path.basename(filePath);
-            fs.stat(filePath, function (error, stats) {
-                if (error) {
-                    console.log('Something went wrong: ', error);
-                    return;
-                }
-                if (stats.isFile()) {
-                    var fileExtension = path.extname(file);
-                    if (extensions.music.includes(fileExtension)) {
-                        fs.mkdir(musicDir, { recursive: true }, function (error) { console.log("Something went wrong: " + error); });
-                        var destPath = path.join(musicDir, fileName);
-                        fs.rename(filePath, destPath, function (error) { console.log("Something went wrong: " + error); });
-                    }
-                    else if (extensions.pictures.includes(fileExtension)) {
-                        fs.mkdir(picDir, { recursive: true }, function (error) { console.log("Something went wrong: " + error); });
-                        var destPath = path.join(picDir, fileName);
-                        fs.rename(filePath, destPath, function (error) { console.log("Something went wrong: " + error); });
-                    }
-                    else if (extensions.documents.includes(fileExtension)) {
-                        fs.mkdir(docDir, { recursive: true }, function (error) { console.log("Something went wrong: " + error); });
-                        var destPath = path.join(docDir, fileName);
-                        fs.rename(filePath, destPath, function (error) { console.log("Something went wrong: " + error); });
-                    }
-                    else if (extensions.video.includes(fileExtension)) {
-                        fs.mkdir(vidDir, { recursive: true }, function (error) { console.log("Something went wrong: " + error); });
-                        var destPath = path.join(vidDir, fileName);
-                        fs.rename(filePath, destPath, function (error) { console.log("Something went wrong: " + error); });
-                    }
-                    else if (extensions.installerEx.includes(fileExtension)) {
-                        fs.mkdir(instDir, { recursive: true }, function (error) { console.log("Something went wrong: " + error); });
-                        var destPath = path.join(instDir, fileName);
-                        fs.rename(filePath, destPath, function (error) { console.log("Something went wrong: " + error); });
-                    }
-                }
-            });
+    return __awaiter(this, void 0, void 0, function () {
+        var files, _i, files_1, file, filePath, fileName, stats, fileExtension, destPath, destPath, destPath, destPath, destPath, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 20, , 21]);
+                    return [4 /*yield*/, fs.promises.readdir(directoryPath)];
+                case 1:
+                    files = _a.sent();
+                    _i = 0, files_1 = files;
+                    _a.label = 2;
+                case 2:
+                    if (!(_i < files_1.length)) return [3 /*break*/, 19];
+                    file = files_1[_i];
+                    filePath = path.join(directoryPath, file);
+                    fileName = path.basename(filePath);
+                    return [4 /*yield*/, fs.promises.stat(filePath)];
+                case 3:
+                    stats = _a.sent();
+                    if (!stats.isFile()) return [3 /*break*/, 18];
+                    fileExtension = path.extname(file);
+                    if (!extensions.music.includes(fileExtension)) return [3 /*break*/, 6];
+                    return [4 /*yield*/, createDirectory(musicDir)];
+                case 4:
+                    _a.sent();
+                    destPath = path.join(musicDir, fileName);
+                    return [4 /*yield*/, renameFile(filePath, destPath)];
+                case 5:
+                    _a.sent();
+                    return [3 /*break*/, 18];
+                case 6:
+                    if (!extensions.pictures.includes(fileExtension)) return [3 /*break*/, 9];
+                    return [4 /*yield*/, createDirectory(picDir)];
+                case 7:
+                    _a.sent();
+                    destPath = path.join(picDir, fileName);
+                    return [4 /*yield*/, renameFile(filePath, destPath)];
+                case 8:
+                    _a.sent();
+                    return [3 /*break*/, 18];
+                case 9:
+                    if (!extensions.documents.includes(fileExtension)) return [3 /*break*/, 12];
+                    return [4 /*yield*/, createDirectory(docDir)];
+                case 10:
+                    _a.sent();
+                    destPath = path.join(docDir, fileName);
+                    return [4 /*yield*/, renameFile(filePath, destPath)];
+                case 11:
+                    _a.sent();
+                    return [3 /*break*/, 18];
+                case 12:
+                    if (!extensions.video.includes(fileExtension)) return [3 /*break*/, 15];
+                    return [4 /*yield*/, createDirectory(vidDir)];
+                case 13:
+                    _a.sent();
+                    destPath = path.join(vidDir, fileName);
+                    return [4 /*yield*/, renameFile(filePath, destPath)];
+                case 14:
+                    _a.sent();
+                    return [3 /*break*/, 18];
+                case 15:
+                    if (!extensions.installer.includes(fileExtension)) return [3 /*break*/, 18];
+                    return [4 /*yield*/, createDirectory(instDir)];
+                case 16:
+                    _a.sent();
+                    destPath = path.join(instDir, fileName);
+                    return [4 /*yield*/, renameFile(filePath, destPath)];
+                case 17:
+                    _a.sent();
+                    _a.label = 18;
+                case 18:
+                    _i++;
+                    return [3 /*break*/, 2];
+                case 19:
+                    console.log('Organizing files complete.');
+                    return [3 /*break*/, 21];
+                case 20:
+                    error_1 = _a.sent();
+                    console.log('Something went wrong: ', error_1);
+                    return [3 /*break*/, 21];
+                case 21: return [2 /*return*/];
+            }
         });
     });
 }
 organizeFiles(directoryPath);
-// /Users/mati/Downloads
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-var fs = require('fs');
-var path = require('path');
-var yargs = require('yargs');
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
 
-var argv = yargs.options({
+const argv = yargs.options({
 	directory: { type: 'string', demandOption: true, alias: 'd' }
 }).argv;
 
-var directoryPath = path.resolve(argv.directory);
+const directoryPath = path.resolve(argv.directory);
 
 const extensions = {
 	music: ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a', '.alac', '.als', '.rpp', '.band', '.mid', '.midi'],
@@ -22,56 +22,70 @@ const docDir = path.join(directoryPath, 'Document');
 const vidDir = path.join(directoryPath, 'Video')
 const instDir = path.join(directoryPath, 'Installers')
 
-function organizeFiles(directoryPath: any) {
-
-	fs.readdir(directoryPath, (error: any, files: any[]) => {
-		if (error) {
-			console.log('Something went wrong: ', error);
-			return;
-		}
-
-		files.forEach((file: any) => {
-
-			const filePath = path.join(directoryPath, file);
-			const fileName = path.basename(filePath)
-
-			fs.stat(filePath, (error: any, stats: { isFile: () => any; }) => {
-				if (error) {
-					console.log('Something went wrong: ', error);
-					return;
-				}
-
-				if (stats.isFile()) {
-
-					const fileExtension = path.extname(file);
-
-					if (extensions.music.includes(fileExtension)) {
-						fs.mkdir(musicDir, { recursive: true }, (error: string) => { console.log("Something went wrong: " + error) })
-						var destPath = path.join(musicDir, fileName)
-						fs.rename(filePath, destPath, (error: string) => { console.log("Something went wrong: " + error) })
-					} else if (extensions.pictures.includes(fileExtension)) {
-						fs.mkdir(picDir, { recursive: true }, (error: string) => { console.log("Something went wrong: " + error) })
-						var destPath = path.join(picDir, fileName)
-						fs.rename(filePath, destPath, (error: string) => { console.log("Something went wrong: " + error) })
-					} else if (extensions.documents.includes(fileExtension)) {
-						fs.mkdir(docDir, { recursive: true }, (error: string) => { console.log("Something went wrong: " + error) })
-						var destPath = path.join(docDir, fileName)
-						fs.rename(filePath, destPath, (error: string) => { console.log("Something went wrong: " + error) })
-					} else if (extensions.video.includes(fileExtension)) {
-						fs.mkdir(vidDir, { recursive: true }, (error: string) => { console.log("Something went wrong: " + error) })
-						var destPath = path.join(vidDir, fileName)
-						fs.rename(filePath, destPath, (error: string) => { console.log("Something went wrong: " + error) })
-					} else if (extensions.installer.includes(fileExtension)) {
-						fs.mkdir(instDir, { recursive: true }, (error: string) => { console.log("Something went wrong: " + error) })
-						var destPath = path.join(instDir, fileName)
-						fs.rename(filePath, destPath, (error: string) => { console.log("Something went wrong: " + error) })
-					}
-				}
-			});
+function createDirectory(directory: any) {
+	return new Promise<void>((resolve, reject) => {
+		fs.mkdir(directory, { recursive: true }, (error: any) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve();
+			}
 		});
 	});
-
 }
 
+function renameFile(sourcePath: any, destPath: any) {
+	return new Promise<void>((resolve, reject) => {
+		fs.rename(sourcePath, destPath, (error: any) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
+async function organizeFiles(directoryPath: any) {
+	try {
+		const files = await fs.promises.readdir(directoryPath);
+
+		for (const file of files) {
+			const filePath = path.join(directoryPath, file);
+			const fileName = path.basename(filePath);
+			const stats = await fs.promises.stat(filePath);
+
+			if (stats.isFile()) {
+				const fileExtension = path.extname(file);
+
+				if (extensions.music.includes(fileExtension)) {
+					await createDirectory(musicDir);
+					const destPath = path.join(musicDir, fileName);
+					await renameFile(filePath, destPath);
+				} else if (extensions.pictures.includes(fileExtension)) {
+					await createDirectory(picDir);
+					const destPath = path.join(picDir, fileName);
+					await renameFile(filePath, destPath);
+				} else if (extensions.documents.includes(fileExtension)) {
+					await createDirectory(docDir);
+					const destPath = path.join(docDir, fileName);
+					await renameFile(filePath, destPath);
+				} else if (extensions.video.includes(fileExtension)) {
+					await createDirectory(vidDir);
+					const destPath = path.join(vidDir, fileName);
+					await renameFile(filePath, destPath);
+				} else if (extensions.installer.includes(fileExtension)) {
+					await createDirectory(instDir);
+					const destPath = path.join(instDir, fileName);
+					await renameFile(filePath, destPath);
+				}
+			}
+		}
+
+		console.log('Organizing files complete.');
+	} catch (error) {
+		console.log('Something went wrong: ', error);
+	}
+}
 
 organizeFiles(directoryPath);
